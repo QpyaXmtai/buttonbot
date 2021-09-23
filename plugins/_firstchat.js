87941c0e266a064517467ef2bb17eedf866f9bd3
sakurakoding <91261477+sakurakoding@users.noreply.github.com>
2021-09-23 17:30:19 +0530

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -12,26 +12,26 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
-`.trim(), '© stikerin', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+${user.banned ? 'kamu dibanned' : 'WELCOME BRO'}
+`.trim(), '© supun', user.banned ? 'owner' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
 
 module.exports = handler
 function ucapan() {
-    const time = moment.tz('Asia/Jakarta').format('HH')
-    res = "Selamat dinihari"
+    const time = moment.tz('Asia/Colombo').format('HH')
+    res = "WELCOME"
     if (time >= 4) {
-        res = "Selamat pagi"
+        res = "ගුඩ් මෝනිං බොම්ක ☕"
     }
     if (time > 10) {
-        res = "Selamat siang"
+        res = "සුබ දහවලක්  SATY HOME 😷"
     }
     if (time >= 15) {
-        res = "Selamat sore"
+        res = "තේකක් බොමුතෙ හවස් වෙලා ☕"
     }
     if (time >= 18) {
-        res = "Selamat malam"
+        res = "ගුඩ් නයිට් 🥱"
     }
     return res
 }
